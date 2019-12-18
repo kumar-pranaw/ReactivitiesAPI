9ebb17c5249535265f99646043a6a9b8d9747e48
technossuspranav <pranav.k.verma@technossus.com>
2019-12-19 01:21:02 +0530

    Seeded value in database and added first migration

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,5 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 namespace DatingApp.API.Controllers
 {
@@ -7,18 +12,26 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var value = await _context.Values.ToListAsync();
+            return Ok(value);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<ActionResult<Value>> Get(int id)
         {
-            return "value";
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
         }
 
         // POST api/values
