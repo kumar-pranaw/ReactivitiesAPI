4d7c1940749fc397b36a235d45d1a8ce9c21d53a
technossuspranav <pranav.k.verma@technossus.com>
2019-12-23 15:33:05 +0530

    Added Interface for the activity model and updated the file structure for the client -app

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
-import { Header, Icon, List } from 'semantic-ui-react'
-import './App.css';
+import { Header, Icon, List } from 'semantic-ui-react';
+import './styles.css';
 import axios from 'axios';
+import {IActivity} from '../model/activity';
 
-class App extends Component {
+interface IState {
+  activities: IActivity[];
+}
 
-  state = {
-    values:  []
+class App extends Component<{}, IState> {
+
+  readonly state: IState = {
+    activities:  []
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
       .then((response) => {
         console.log(response);
         this.setState({
-          values: response.data
+          activities: response.data
         })
       })
     
@@ -28,9 +33,8 @@ class App extends Component {
              <Header.Content>Reactivities</Header.Content>
           </Header>
           <List>
-              {this.state.values.map((value: any) => (
-                <List.Item key={value.id}> {value.name}  </List.Item>))}
-            
+              {this.state.activities.map((activity) => (
+                <List.Item key={activity.id}> {activity.title}  </List.Item>))}
           </List>
 
          <ul>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import  './app/layout/styles.css';
 import * as serviceWorker from './serviceWorker';
+import App from './app/layout/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
