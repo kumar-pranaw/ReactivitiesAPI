60a19a97d0a342e442b7719fbe084ed21aa83b43
technossuspranav <pranav.k.verma@technossus.com>
2020-01-06 23:42:26 +0530

    Added Attendee on UI Under Activity

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -3,8 +3,10 @@ import { Item, Button, Segment, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { IActivity } from '../../../app/models/activity';
 import { format } from "date-fns";
+import ActivityListItemAttendees from './ActivityListItemAttendees';
 
 const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
+  console.log(activity.attendees);
   return (
     <Segment.Group>
       <Segment>
@@ -22,7 +24,7 @@ const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
         <Icon name='clock' /> {format(activity.date, 'h:mm a')}
         <Icon name='marker' /> {activity.venue}, {activity.city}
       </Segment>
-      <Segment secondary>Attendees will go here</Segment>
+      <Segment secondary><ActivityListItemAttendees attendees = {activity.attendees}/></Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
